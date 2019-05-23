ac823375028c07308effc1498bf4541eafbb5e04
tarun.sharma <tarun.sharma@diversey.com>
2019-05-23 15:41:53 +0530

    added data

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,15 @@
-const UTILS = {
+let d = new Date();
+let curr_day = d.getDay();
+let curr_date = d.getDate();
+let sup = "";
+let curr_month = d.getMonth();
+let curr_year = d.getFullYear();
+let curr_hour = d.getHours();
+let curr_min = d.getMinutes();
+let curr_sec = d.getSeconds();
+
+let UTILS = {
   getFormattedDateTime: () => {
-    return `${d_names[curr_day]} ${curr_date}<SUP>${sup}</SUP> ${m_names[curr_month]} ${curr_year}
-    ${curr_hour}:${curr_min}:${curr_sec}`;
+    return `${CONSTANTS.d_names[curr_day]} ${curr_date}<SUP>${sup}</SUP> ${CONSTANTS.m_names[curr_month]} ${curr_year}<br>${curr_hour}:${curr_min}:${curr_sec}`;
   }
 }
